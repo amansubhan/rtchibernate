4c5e928ace008c58c2b037cc5ff0c7c3bc4642ff
Aman Subhan <amansubhan929@gmail.com>
2015-10-16 10:55:54 +0500

    fixed issue with timestamp

--- rtchibernate.sh
+++ rtchibernate.sh
@@ -7,18 +7,18 @@ echo "$timestamp $1" >> /tmp/autohibernate.log
 
 if [ "$1" = "pre" ]
 then
-    echo "$timestamp" > /var/run/pm-utils/locks/rtchibernate.lock
+    echo "$timestamp" > /var/lock/rtchibernate.lock
     rtcwake -m no -s $autohibernate
 fi
 
 if [ "$1" = "post" ]
 then
-    sustime=$(cat /var/run/pm-utils/locks/rtchibernate.lock)
-    rm /var/run/pm-utils/locks/rtchibernate.lock
+    sustime=$(cat /var/lock/rtchibernate.lock)
+    rm /var/lock/rtchibernate.lock
 
-    if [ $(($curtime - $sustime)) -ge $autohibernate ]
+    if [ $(($timestamp - $sustime)) -ge $autohibernate ]
     then
-        rm /var/run/pm-utils/locks/pm-suspend.lock
+        #rm /var/run/pm-utils/locks/pm-suspend.lock
         systemctl hibernate
     else
         rtcwake -m no -s 1
